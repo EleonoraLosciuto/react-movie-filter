2b385b8b2de1d6f90a8c0749f525c65e7183b9ed
Eleonora Losciuto <eleonora.losciuto@gmail.com>
2026-08-12 18:10:52 +0200

    add: input for filter movie title and handleInput function

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,30 @@ const initialMovies = [
 function App() {
   const [movies, setMovies] = useState(initialMovies);
   const [filteredMovies, setFilteredMovies] = useState(movies);
+  const [filter, setFilter] = useState("");
+
+  const handleFilterInput = (e) => (
+    setFilter(e.target.value),
+    console.log(filter)
+  );
 
   return (
-    movies.map((movie) => (
-      <div className='movie-card'>
-        <p className='movie-title'>{movie.title}</p>
-        <p className='movie-genre'>{movie.genre}</p>
-      </div>
-    )
-    )
+    <>
+      <header>
+        <div className="filterBar">
+          <label>Cerca film: </label>
+          <input type="text" onChange={handleFilterInput} value={filter} />
+        </div>
+      </header>
+
+      {movies.map((movie, index) => (
+        <div className='movie-card' key={index}>
+          <p className='movie-title'>{movie.title}</p>
+          <p className='movie-genre'>{movie.genre}</p>
+        </div>
+      )
+      )}
+    </>
   )
 };
 
